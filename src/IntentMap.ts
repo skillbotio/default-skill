326a8bc04e2df6233fbb0ceb6e75fbb2e6611136
John Kelvie <john.kelvie@gmail.com>
2017-11-07 11:22:52 -0500

    Added more tabs

--- src/IntentMap.ts
+++ src/IntentMap.ts
@@ -14,14 +14,14 @@ export class IntentMap {
 const map: {[id: string]: string[]} = {
     "AMAZON.HelpIntent": [
         "Core Commands:\n" +
-        "   open:\tLaunch a skill by saying \"open SKILL_NAME\"\n" +
+        "\topen:\tLaunch a skill by saying \"open SKILL_NAME\"\n" +
         "\t\t\tSynonyms: ask, tell and launch\n" +
-        "   skills:\tList skills that are available\n\n" +
-        "Debug Commands:\n" +
-        "   associate: Claim ownership a skill with \"associate SKILL_ID\"\n" +
-        "   debug:     Enable debuging of skill - shows requests and responses\n" +
-        "              Skill must be associated first\n" +
-        "   debug off: Turn off debugging\n"],
+        "\tskills:\tList skills that are available\n\n" +
+        "\tDebug Commands:\n" +
+        "\tassociate:\tClaim ownership a skill with \"associate SKILL_ID\"\n" +
+        "\tdebug:\t\tEnable debuging of skill - shows requests and responses\n" +
+        "\t\t\tSkill must be associated first\n" +
+        "\tdebug off: Turn off debugging\n"],
     "Hello": ["Hi!", "Howdy!", "Hi there!"],
     "Who": ["I am SkillBot - I allow you to interact with Alexa skills from Slack"],
 };
